c01f66c6fa1f247fc1647759b81d9015fae19da2
Ricardo Rubio <rickarubio@gmail.com>
2014-03-10 11:37:40 -0700

    Add rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bartschedule::Application.config.secret_token = '8b4cea3c76de83408e60c37d556edb02728c0d176a58d6b14859436f04511210264ce2c3297813f52065d0ffc56373726f7474e14e53ba24df698e0088412ae1'
